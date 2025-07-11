44d2ea1a9bee3cbd8c184f25f5c769354af5bd5c
Quvonchbek <1.quvonchbek.ibragimov@gmail.com>
2025-07-11 11:45:21 +0500

    leaderboard update

--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'leaderboard'
 
 urlpatterns = [
+    path('', views.leaderboards_home, name='leaderboards_home'),
     path('<slug:game_slug>/', views.game_leaderboard, name='game_leaderboard'),
 ]
--- leaderboard/views.py
+++ leaderboard/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from games.models import Game
 
+
+def leaderboards_home(request):
+    games = Game.objects.filter(is_active=True)
+    return render(request, 'leaderboard/leaderboards_home.html', {
+        'games': games,
+    })
+
+
 def game_leaderboard(request, game_slug):
     game = get_object_or_404(Game, slug=game_slug)
     scores = game.gamescore_set.order_by('-score')[:10]
