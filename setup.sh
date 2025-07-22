35bfbb888399ff38ca3dfa84c70d995e407d579f
Quvonchbek <1.quvonchbek.ibragimov@gmail.com>
2025-07-22 08:49:42 +0500

    gmail changed

--- setup.sh
+++ setup.sh
@@ -13,11 +13,11 @@ echo "Requesting Let's Encrypt certificate with Certbot..."
 docker-compose run --rm certbot certonly \
   --webroot \
   --webroot-path=/var/www/certbot \
-  --email "$EMAIL" \
   --agree-tos \
-  --no-eff-email \
   --non-interactive \
-  -d "$DOMAIN" -d "www.$DOMAIN"
+  --register-unsafely-without-email \
+  -d nksgamezone.uz -d www.nksgamezone.uz
+
 
 if [ $? -eq 0 ]; then
   echo "Certificate obtained successfully!"
