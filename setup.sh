2b81ba32e4d6a80efb0e33399bef4a316d0c2f41
Quvonchbek <1.quvonchbek.ibragimov@gmail.com>
2025-07-22 08:46:18 +0500

    gmail changed

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-EMAIL="testlinux659@gmail.com"
+EMAIL="1.quvonchbek.ibragimov@gmail.com"
 DOMAIN="nksgamezone.uz"
-DOMAINS="-d $DOMAIN -d www.$DOMAIN"
 
 echo "Starting nginx and app containers..."
 docker-compose up -d nginx app
@@ -11,16 +10,19 @@ echo "Waiting for nginx to be ready..."
 sleep 5
 
 echo "Requesting Let's Encrypt certificate with Certbot..."
-docker-compose run --rm certbot certonly --webroot --webroot-path=/var/www/certbot \
-  --email $EMAIL --agree-tos --no-eff-email $DOMAINS
+docker-compose run --rm certbot certonly \
+  --webroot \
+  --webroot-path=/var/www/certbot \
+  --email "$EMAIL" \
+  --agree-tos \
+  --no-eff-email \
+  --non-interactive \
+  -d "$DOMAIN" -d "www.$DOMAIN"
 
 if [ $? -eq 0 ]; then
   echo "Certificate obtained successfully!"
-  echo "Restarting nginx to apply new certificates..."
   docker-compose restart nginx
 else
   echo "Failed to obtain certificate. Please check the logs."
   exit 1
 fi
-
-echo "Setup complete. Your site should be available at https://$DOMAIN"
